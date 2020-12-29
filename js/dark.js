4c0c9969fb10fd45ed9c7036bacd009178ed33d7
HenriqueRib <ribeiro.henriquem@gmail.com>
2020-12-29 09:02:42 -0300

    Dark mode funcionando

--- js/dark.js
+++ js/dark.js
@@ -1,22 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModeStorage = localStorage.getItem('dark-mode')
+  const body = document.querySelector('body')
+  const inputDarkMode = document.getElementById('input-dark-mode')
 
-      document.addEventListener('DOMContentLoaded', () => {
-        const darkModeStorage = localStorage.getItem('dark-mode')
-        const html = document.querySelector('body')
-        const inputDarkMode = document.getElementById('input-dark-mode')
-  
-        if(darkModeStorage){
-          html.setAttribute("dark", "true")
-        }
-  
-        inputDarkMode.addEventListener('change', () => {
-          if(inputDarkMode.checked){
-            html.setAttribute("dark", "true")
-            localStorage.setItem('dark-mode', true)
-          }else{
-            html.removeAttribute("dark")
-            localStorage.removeItem('dark-mode')
-          }
-        })
-  
-      })
-  
+  if(darkModeStorage){
+    body.setAttribute("dark", "false")
+  }
+
+  inputDarkMode.addEventListener('change', () => {
+    if(inputDarkMode.checked){
+      body.setAttribute("dark", "true")
+      localStorage.setItem('dark-mode', true)
+    }else{
+      body.removeAttribute("dark")
+      localStorage.removeItem('dark-mode')
+    }
+  })
+
+})
